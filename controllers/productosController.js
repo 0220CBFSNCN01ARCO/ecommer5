be516a440a2f235deafa59693c7da06b53a200be
francoponce-cloud <franponce98@gmail.com>
2020-06-10 02:14:56 -0300

    arreglos

--- controllers/productosController.js
+++ controllers/productosController.js
@@ -1,26 +1,43 @@
 const fs = require('fs');
 const path = require("path");
-const productsPath = path.join(__dirname, '../data/detalleProductos.json');
+const productsPath = path.join(".", "data", "detalleProductos.json");
 const listadoProductos = JSON.parse(fs.readFileSync('./data/detalleProductos.json', 'utf-8'));
 
 const productosController = {
-"listado" : function(req, res){
+listado : function(req, res){
     res.render("listadoProductos", {"listadoProductos": listadoProductos});
 },
-"detalle" : function(req, res){
+detalle : function(req, res){
 let idProductoElegido = req.params.idProduct;
 const productoAMostrar = listadoProductos.find(producto => {
-    return listadoProductos.idProduct == productoAMostrar
+    return listadoProductos.idProduct == idProductoElegido
 });
     res.render("detalleProducto", {"productoClickeado": productoAMostrar});
 },
-"create" : function(req, res){
-    res.render("agregarProducto");
+create : function(req, res){
+    let producto = {
+        id: req.body.id,
+        titulo: req.body.titulo,
+        categoria: req.body.categoria
+    }
+    let archivoProductos = fs.readFileSync("detalleProductos.json", {encoding: "utf-8"});
+    let productos;
+if(archivoProductos == ""){
+   productos = [];
+}else{
+   productos = JSON.parse(archivoProductos);
+}
+usuarios.push(producto);
+
+let productoJSON = JSON.stringify(productos);
+fs.appendFileSync("detalleProductos.json", productoJSON);
+
+    res.redirect("agregarProducto", {titulo: "Se agregó un nuevo producto"});
 },
-"update": function(req, res){
+update: function(req, res){
     res.render("")
 },
-"delete": function(req, res){
+delete: function(req, res){
 
     res.redirect("/product")
 },
--- controllers/registroController.js
+++ controllers/registroController.js
@@ -1,9 +1,30 @@
+const fs = require('fs');
+
 const registroController = {
-    register: function (req, res) {
-        res.render('users');
+  register : function(req, res){
+    res.render("users")
+  },
+    create: function (req, res) {
+let usuario = {
+nombre: req.body.nombre,
+email: req.body.email,
+password: req.body.password
+}
+let archivoUsuarios = fs.readFileSync("users.json", {encoding: "utf-8"});
+let usuarios;
+if(archivoUsuarios == ""){
+   usuarios = [];
+}else{
+   usuarios = JSON.parse(archivoUsuarios);
+}
+usuarios.push(usuario);
+
+let usuarioJSON = JSON.stringify(usuarios);
+fs.appendFileSync("users.json", usuariosJSON);
+
+        res.redirect('users', {title: "Gracias por registrarte en Mercado Libro"});
       }
+}
 
-
-};
 
 module.exports = registroController;
