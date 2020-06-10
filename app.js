a3254f8458a89b8db9bda73426fb6fbe2903ddef
francoponce-cloud <franponce98@gmail.com>
2020-06-10 15:23:37 -0300

    middleware log - url, express validator, cambio en routes de users

--- app.js
+++ app.js
@@ -7,12 +7,15 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var productRouter = require('./routes/product');
+var logMiddleware = require('./middleware/logMiddleware');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(logMiddleware);
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const registroController = require("../controllers/registroController")
+let logDBMiddleware = require('../middleware/logDBMiddleware');
+let { check, validationResult, body } = require('express-validator');
 
-router.get('/', registroController.register);
-router.post('/', registroController.create);
+router.get('/register', registroController.register);
+router.post('/register', logDBMiddleware, [
+    check('nombre').isLength(),
+    check('prov').isLength(),
+    check('localidad').isLength(),
+    check('direccion').isLength(),
+    check('cp').isLength(),
+    check('numero').isLength(),
+    check('email').isEmail(),
+    check('contraseña').isLength({min: 8})
+] ,registroController.create);
 
 module.exports = router;
