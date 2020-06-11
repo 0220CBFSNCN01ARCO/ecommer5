06fd738a5ed3aa550f7394087fa888c73b543253
yasucaria <ya_sucaria@hotmail.com>
2020-06-11 18:01:09 -0300

    detalles

--- middleware/logDBMiddleware.js
+++ middleware/logDBMiddleware.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 function logDBMiddleware(req, res, next) {
-    fs.appendFileSync('logDB.txt', 'Se creo un registro al ingresar enurl  ' + req.url)
+    fs.appendFileSync('logDB.txt', 'Se creo un registro al ingresar en url  ' + req.url)
 
     next();
 }
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const registroController = require("../controllers/registroController");
+let fs = require("fs");
+const listadoUsers = JSON.parse(fs.readFileSync('./data/users.json', 'utf-8'));
+const usersController = require("../controllers/usersController");
 let logDBMiddleware = require("../middleware/logDBMiddleware");
 let { check, validationResult, body } = require("express-validator");
-let fs = require("fs");
 
-router.get("/", registroController.register);
+
+router.get("/register", usersController.register);
 router.post(
-  "/",
-  logDBMiddleware,
+  "/register",
+ //logDBMiddleware,
   [
     check("nombre").isLength().withMessage("Este campo debe estar completo"),
     check("prov").isLength().withMessage("Te falto la provincia"),
@@ -16,26 +18,28 @@ router.post(
     check("direccion").isLength().withMessage("Y la dirección?"),
     check("cp").isLength().withMessage("Sin codigo postal no te encuentro"),
     check("numero").isLength().withMessage("Sin número no te encuento"),
-    check("email").isEmail().withMessage("El email debe ser uno valido"),
-    check("contraseña")
-      .isLength({ min: 8 })
-      .withMessage("Hasta 8 caracteres podés poner"),
-    body("email").custom(function (value) {
-      let users;
-      if (userJSON == "") {
-        users = [];
+    check("email").isEmail().custom(function (value) {
+     let listadoUsers = fs.readFileSync("users.json", {encoding: "utf-8"});
+     console.log(listadoUsers)
+     let users;
+     if (listadoUsers == "") {
+       users = [];
       } else {
-        users = JSON.parse(userJSON);
+       users = JSON.parse(listadoUsers);
       }
 
       for (let i = 0; i < users.length; i++) {
-        if (user[i].email == value) return false;
+        if (users[i].email == value) return false;
       }
       return true;
-    }).withMessage('Usuario ya existente')
+    }).withMessage('Usuario ya existente'),
+    check("contraseña")
+      .isLength({ min: 8 })
+      .withMessage("Hasta 8 caracteres podés poner")
+  
   ],
-  registroController.create
+  usersController.create
 );
-router.get("/login", registroController.login);
+router.get("/login", usersController.login);
 
 module.exports = router;
