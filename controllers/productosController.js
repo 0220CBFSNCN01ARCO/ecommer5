ff7751d35681efd23a37402a2b89adbed40a4cbb
yasucaria <ya_sucaria@hotmail.com>
2020-07-01 19:45:23 -0300

    se refactoriza codigo de listado y agregar producto en controlador - crashea

--- controllers/productosController.js
+++ controllers/productosController.js
@@ -27,38 +27,45 @@ res.render("createProduct")
 
 agregar: function(req, res){
    
-    let errors = validationResult(req);
+    //let errors = validationResult(req);
 
-    if(errors.isEmpty()){
-      
-      let productosJSON = fs.readFileSync("./data/detalleProductos.json", {encoding: "utf-8"});
-      let productos;
-      if (productosJSON == "") {
-        productos = [];
-      } else {
-      productos = JSON.parse(productosJSON);
-  
-      }
-      let producto = {
+    //if(errors.isEmpty()){
+      db.Libro.create({
         titulo: req.body.titulo,
         autor: req.body.autor,
         categoria: req.body.categoria,
         precio: req.body.precio,
         stock: req.body.sotck,
-        avatar: req.files[0].filename,
+        avatar: req.files[0].filename
+      })
+      //let productosJSON = fs.readFileSync("./data/detalleProductos.json", {encoding: "utf-8"});
+      //let productos;
+      //if (productosJSON == "") {
+      //  productos = [];
+      //} else {
+      //productos = JSON.parse(productosJSON);
   
-      }
+     // }
+     // let producto = {
+      //  titulo: req.body.titulo,
+       // autor: req.body.autor,
+       // categoria: req.body.categoria,
+      //  precio: req.body.precio,
+      //  stock: req.body.sotck,
+      //  avatar: req.files[0].filename,
   
-      productos.push(producto);
+     // }
   
-      productosJSON = JSON.stringify(productos);
+      //productos.push(producto);
   
-      fs.appendFileSync("./data/detalleProductos.json", productosJSON);
+      //productosJSON = JSON.stringify(productos);
+  
+      //fs.appendFileSync("./data/detalleProductos.json", productosJSON);
   
       res.redirect('/products');
-    } else {
-      res.render("createProduct", {errors: errors.errors})
-    }
+   // } else {
+    //  res.render("createProduct", {errors: errors.errors})
+   // }
 
 
 },
