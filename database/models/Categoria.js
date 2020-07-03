4f9e2bf1e66b2e827d8e6ab42e6bad646591abd1
yasucaria <ya_sucaria@hotmail.com>
2020-07-03 10:34:23 -0300

    corrijo sintaxis de modelo Categoria

--- database/models/Categoria.js
+++ database/models/Categoria.js
@@ -1,4 +1,5 @@
 module.exports = function(sequelize, DataTypes) {
+
     let alias = "Categoria";
 
     let cols = {
@@ -8,29 +9,34 @@ module.exports = function(sequelize, DataTypes) {
             autoIncrement: true
         },
         nombre: {
-            type: DataTypes.VARCHAR//(45)
+            type: DataTypes.STRING//(45)
         },
         idLibro: {
             type: DataTypes.INTEGER//(11)
         }
     }
-}
 
     let config = {
         tableName: "categorias",
         timestamps: false
     }
-
     let Categoria = sequelize.define(alias, cols, config);
+
+    Categoria.associate = function(models) {
+        Categoria.hasMany(models.Libro, {
+                as: "libros",
+                foreignKey: "idLibro",
+                timestamps: false
+                });
+        }
+
+        return Categoria;
+}
+
+    
+
+    
 
 
 // relacion uno a muchos. Una categoría tiene muchos libros
-    Categoria.associate = function(models) {
-    Categoria.hasMany(models.Libro, {
-            as: "libros",
-            foreignKey: "idLibro",
-            timestamps: false
-            });
-    }
-
-    return Categoria;
+   
