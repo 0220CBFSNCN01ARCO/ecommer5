bef1546dcb4c9d5d3d8f575098225d4965e6ddfe
yasucaria <ya_sucaria@hotmail.com>
2020-07-03 10:57:26 -0300

    paso la ruta de CRUD de productos a la de admin incluyendo metodos en controlador

---
+++ routes/admin.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+const adminController = require('../controllers/adminController')
+const multer = require('multer');
+
+var storage = multer.diskStorage({
+    destination: function(req, file, cb){
+      cb(null, "tmp/my-uploads")
+    } ,
+    filename: function(req, file, cb){
+      cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname))
+    }
+  });
+  var upload = multer({storage: storage});
+
+  const adminController = require("../controllers/adminController");
+  let adminMiddleware = require("../middleware/admin");
+  let { check, validationResult, body } = require("express-validator");
+let fs = require("fs");
+
+
+
+router.get('/', adminMiddleware,  adminController.adminProducts);
+router.get('/create', adminController.create);
+
+router.post('/create', upload.any(), [
+    check("titulo").isLength({min: 4}).withMessage("Falta el título del libro"),
+    check("autor").isLength().withMessage("Falta aclarar el autor"),
+    check("precio").isInt().withMessage("El producto no tiene precio"),
+    check("stock").isInt().withMessage("Falta aclarar el stock")
+], adminController.agregar);
+
+router.get('/edit/:id', adminController.edit);
+router.post('/edit/:id', adminController.update);
+router.delete('/delete/:id',adminController.delete);
+
+
+module.exports = router;
--- routes/product.js
+++ routes/product.js
@@ -1,33 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const multer = require("multer");
-let path = require("path");
-let authMiddleware = require("../middleware/authMiddleware");
-let { check, validationResult, body } = require("express-validator");
-var storage = multer.diskStorage({
-  destination: function(req, file, cb){
-    cb(null, "tmp/my-uploads")
-  } ,
-  filename: function(req, file, cb){
-    cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname))
-  }
-});
-var upload = multer({storage: storage});
+
 const productosController = require("../controllers/productosController.js");
 
 router.get("/", productosController.listado);
 router.get("/detail/:id", productosController.detail);
-router.get("/create", productosController.create);
 
-router.post("/create", upload.any(),[
-    check("titulo").isLength({min: 4}).withMessage("Falta el título del libro"),
-    check("autor").isLength().withMessage("Falta aclarar el autor"),
-    check("precio").isInt().withMessage("El producto no tiene precio"),
-    check("stock").isInt().withMessage("Falta aclarar el stock")
-], productosController.agregar);
-
-router.get("/edit/:id", productosController.edit);
-router.post("/edit/:id", productosController.update);
-router.delete("/delete/:id", productosController.delete);
 
 module.exports = router;
