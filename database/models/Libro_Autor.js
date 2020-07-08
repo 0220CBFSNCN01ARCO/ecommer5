27ab586637f12178d987a78d65c0b9fccf9bd64c
francoponce-cloud <franponce98@gmail.com>
2020-07-08 12:44:32 -0300

    Se estructura modelo Libro_Autor

--- database/models/Libro_Autor.js
+++ database/models/Libro_Autor.js
@@ -1,26 +1,48 @@
-module.exports = function(sequelize, DataTypes) {
-    let alias = "Libro_Autor";
+module.exports = function (sequelize, DataTypes) {
+  let alias = "Libro_Autor";
 
-    let cols = {
-        id: {
-            type: DataTypes.INTEGER,//(11)
-            primaryKey: true,
-            autoIncrement: true
-        },
-        idLibro: {
-            type: DataTypes.INTEGER//(11)
-        },
-        idAutor: {
-            type: DataTypes.INTEGER//(11)
-        }
-     }
+  let cols = {
+    id: {
+      type: DataTypes.INTEGER, //(11)
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    idLibro: {
+      type: DataTypes.INTEGER, //(11)
+    },
+    idAutor: {
+      type: DataTypes.INTEGER, //(11)
+    },
+  };
 
-    let config = {
-        tableName: "libros_autores",
-        timestamps: false
-    }
+  let config = {
+    tableName: "libros_autores",
+    timestamps: false,
+  };
 
-    let Libro_Autor = sequelize.define(alias, cols, config);
-    return Libro_Autor
+  let Libro_Autor = sequelize.define(alias, cols, config);
+  return Libro_Autor;
+};
 
-}
+module.exports = (sequelize, DataTypes) => {
+  const Libro_Autor =
+    ("Libro_Autor",
+    {
+      id: {
+        type: DataTypes.INTEGER, //(11)
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      idLibro: {
+        type: DataTypes.INTEGER, //(11)
+      },
+      idAutor: {
+        type: DataTypes.INTEGER, //(11)
+      },
+    },
+    {
+      tableName: "libros_autores",
+      timestamps: false,
+    });
+  return Libro_Autor;
+};
