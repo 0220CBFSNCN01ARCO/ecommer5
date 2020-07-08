343dec2c1728ce2807073bfd5d92b4e454f505d1
francoponce-cloud <franponce98@gmail.com>
2020-07-08 12:45:25 -0300

    Se estructura modelo Libro_Autor

--- database/models/Libro_Autor.js
+++ database/models/Libro_Autor.js
@@ -1,29 +1,3 @@
-module.exports = function (sequelize, DataTypes) {
-  let alias = "Libro_Autor";
-
-  let cols = {
-    id: {
-      type: DataTypes.INTEGER, //(11)
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    idLibro: {
-      type: DataTypes.INTEGER, //(11)
-    },
-    idAutor: {
-      type: DataTypes.INTEGER, //(11)
-    },
-  };
-
-  let config = {
-    tableName: "libros_autores",
-    timestamps: false,
-  };
-
-  let Libro_Autor = sequelize.define(alias, cols, config);
-  return Libro_Autor;
-};
-
 module.exports = (sequelize, DataTypes) => {
   const Libro_Autor =
     ("Libro_Autor",
