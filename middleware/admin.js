e1198b8c50989034fe52e77a3b9041e5c88f7787
yasucaria <ya_sucaria@hotmail.com>
2020-07-12 10:42:15 -0300

    agrego avatar de admin en profileAdmin

--- middleware/admin.js
+++ middleware/admin.js
@@ -2,6 +2,7 @@ const verifyAdmin = function(req, res, next){
     let usersAdmin = {
         nombre: "Yael Sucaria",
         email: "ya_sucaria@hotmail.com",
+        avatar: "user-1594249373228.png"
     }
    let usuarioLogueado;
       if(req.body.email == usersAdmin.email) {
