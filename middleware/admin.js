2f1554d8c796bd2eec81bd8533226ed159b49a9a
yasucaria <ya_sucaria@hotmail.com>
2020-07-13 07:03:34 -0300

    mejoro validacion de login de admin en el middleware admin

--- middleware/admin.js
+++ middleware/admin.js
@@ -2,13 +2,17 @@ const verifyAdmin = function(req, res, next){
     let usersAdmin = {
         nombre: "Yael Sucaria",
         email: "ya_sucaria@hotmail.com",
+        password: "lemebel2512",
         avatar: "user-1594249373228.png"
     }
    let usuarioLogueado;
-      if(req.body.email == usersAdmin.email) {
+      if(req.body.email == usersAdmin.email && req.body.password == usersAdmin.password) {
           let usuarioLogueado = usersAdmin;
          res.render("profileAdmin", {usuarioLogueado});
-            } else {
+            } 
+        if(req.body.email == usersAdmin.email && req.body.password != usersAdmin.password){
+            res.send("Contraseña incorrecta")
+        } else {
                 next()
             }       
 };
