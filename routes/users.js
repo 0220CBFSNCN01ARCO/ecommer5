b8edea8fddcd63d1a40e43091a6a3b44d661efa3
yasucaria <ya_sucaria@hotmail.com>
2020-07-16 22:13:42 -0300

    dejo funcionando validacion por body custom de registracion y login

--- routes/users.js
+++ routes/users.js
@@ -26,26 +26,26 @@ router.get("/register", guestMiddleware, usersController.register);
 
 router.post("/register", upload.any(), [
     check("nombre").isLength({min: 4}).withMessage("Me falta tu nombre y apellido"),
-    check("provincia").isLength({min: 4}).withMessage("Te faltó la provincia"),
+    check("prov").isLength({min: 4}).withMessage("Te faltó la provincia"),
     check("localidad").isLength({min: 1}).withMessage("Te faltó la localidad"),
     check("direccion").isLength({min: 1}).withMessage("Y la dirección?"),
     check("cp").isInt({min: 0}).withMessage("Sin codigo postal no te encuentro"),
     check("email").isEmail().withMessage("Falta tu email"),
-   // body("email").custom(function (value){
-   //  db.Usuario.findOne({
-    //   where: {
-    //     email : value
-    //   }
-    // })
-    // .then(function(usuario){
-    //   if(usuario == null || usuario == undefined){
-//return false
-     //  } else {
-     //    return true
-     //  }
-    // })
-  //  }
-   // ).withMessage("Usuario ya existente"),
+   body("email").custom(function (value){
+   db.Usuario.findOne({
+    where: {
+      email : value
+     }
+    })
+    .then(function(usuario){
+     if(!usuario){
+return false
+      } else {
+       return true
+      }
+   })
+  }
+    ).withMessage("Usuario ya existente"),
     check("password").isLength({min: 8}).withMessage("La contraseña debe tener 8 caracteres como mínimo") 
 
   ], usersController.create);
@@ -55,22 +55,21 @@ router.get("/login", usersController.login);
 
 router.post("/login", /*validationusers*/ adminMiddleware.verifyAdmin, [
   check("email").isEmail().withMessage("Email inválido"),
-  check("password").isLength({min: 1}).withMessage("La contraseña debe tener al menos 8 caracteres")//,
- // body("email").custom(function(value){
-  //  let usuariosJSON = fs.readFileSync('./data/users.json', {encoding:'utf-8'});
-   // let users;
- //   if(usuariosJSON == ""){
-    //  users = [];
-   // }else{
-   //   users = JSON.parse(usuariosJSON);
-   // }
-  //  for(let i = 0;i< users.length ;i++){
-  //    if(users[i].email == value){
-    //    return true;
-   //  }
-   // }
-   // return false;
- //}).withMessage("No tenemos registrado tu email")
+  body("email").custom(function(value){
+    db.Usuario.findOne({
+      where: {
+        email : value
+       }
+      })
+      .then(function(usuario){
+        if(usuario){
+   return true
+         } else {
+          return false
+         }
+      })
+ }).withMessage("No tenemos registrado tu email"),
+ check("password").isLength({min: 1}).withMessage("La contraseña debe tener al menos 8 caracteres")
 ], usersController.processLogin); 
 
 router.get('/check', function(req, res) {
