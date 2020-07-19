1e93722142096a4cc983572beb983531a62b8eb2
yasucaria <ya_sucaria@hotmail.com>
2020-07-19 19:46:43 -0300

    dejo ok la validacion de edicion y eliminacion de productos con mensaje confirm

--- public/essence/javascript/js-front.js
+++ public/essence/javascript/js-front.js
@@ -1,30 +1,27 @@
-//const db = require("../../../database/models")
-
 window.addEventListener("load", function(){
-console.log("Hola")
-
-let iconoDelete = document.querySelector(".iconodelete");
-iconoDelete.addEventListener("click", function(event){
-   
-let confirmDelete =  confirm("¿Estás seguro de querer eliminar este producto?")
-
-    if(confirmDelete == false){
-        event.preventDefault();
-    } 
+    console.log("Hola")
     
+    let iconoDelete = document.querySelector(".iconodelete");
+    iconoDelete.addEventListener("click", function(e){
 
-})
+        let confirmDelete = confirm("¿Estás seguro de querer eliminar este producto?")
+        
+            if(confirmDelete == false){
+                e.preventDefault()
+            } 
+        
+        
+        })
+        
+        let iconoEdit = document.querySelector(".iconoedit")
+        iconoEdit.addEventListener("click", function(e){
+            let confirmEdit = confirm("¿Estás seguro de querer editar este producto?")
 
-let iconoEdit = document.querySelector(".iconoedit");
-iconoEdit.addEventListener("click", function(event){
-    
-    let confirmEdit =  confirm("¿Estás seguro de querer editar este producto?")
-   
-        if(confirmEdit == false){
-            event.preventDefault();
-        }
-    
-    
-})
+            if(confirmEdit == false){
+                e.preventDefault()
+            }
+        })
 
-})
+
+
+    })
