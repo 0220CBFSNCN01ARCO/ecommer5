660444ab609b9c6825cc362c377a85ebc77cb489
yasucaria <ya_sucaria@hotmail.com>
2020-07-19 18:53:12 -0300

    se deja funcionando mensaje confirm para edit y delete product - queda ok

--- public/essence/javascript/js-front.js
+++ public/essence/javascript/js-front.js
@@ -1,42 +1,29 @@
-const db = require("../../../database/models")
+//const db = require("../../../database/models")
 
 window.addEventListener("load", function(){
 console.log("Hola")
 
 let iconoDelete = document.querySelector(".iconodelete");
-iconoDelete.addEventListener("click", function(e){
-    e.preventDefault();
-let confirmDelete =  confirm("¿Estás seguro de querer eliminar este producto?");
-let functionDelete = function(req, res){
+iconoDelete.addEventListener("click", function(event){
+   
+let confirmDelete =  confirm("¿Estás seguro de querer eliminar este producto?")
+
     if(confirmDelete == false){
-        res.send("No eliminaste nada");
-    } else {
-        db.Libro.update({
-            titulo: req.body.titulo,
-            autor: req.body.autor,
-            precio: req.body.precio,
-            stock: req.body.stock,
-            descripcion: req.body.descripcion,
-            portada: req.body.filename
-          }, { where: {
-              idlibros: req.params.idlibros
-          }
-        })
-    }
-}
+        event.preventDefault();
+    } 
+    
+
 })
 
 let iconoEdit = document.querySelector(".iconoedit");
-iconoEdit.addEventListener("click", function(e){
-    e.preventDefault();
+iconoEdit.addEventListener("click", function(event){
+    
     let confirmEdit =  confirm("¿Estás seguro de querer editar este producto?")
-   let functionEdit = function(req, res){
+   
         if(confirmEdit == false){
-            res.render("profileAdmin");
-        }else{
-          
+            event.preventDefault();
         }
-    }
+    
     
 })
 
