30e6e54a231debbd7401d7b36793a83e06781b85
francoponce-cloud <franponce98@gmail.com>
2020-08-03 10:03:34 -0300

    Se agrega class agregar-carrito y crea logica inicial carrito

--- public/essence/js/carrito.js
+++ public/essence/js/carrito.js
@@ -1,1 +1,10 @@
-﻿
+﻿class Carrito{
+    //Añadir el producto al carrito
+    comprarProducto(e){
+        e.preventDefault();
+        if(e.target.classList.contains('agregar-carrito')){
+            const producto = e.target.parentElement.parentElement;
+            this.leerDatosProducto(producto);
+        }
+    }
+}
