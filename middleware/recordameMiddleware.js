4f3a85e6f9c9561801c3ba6c6ed952435038a032
francoponce-cloud <franponce98@gmail.com>
2020-08-09 14:17:13 -0300

    Sin cambios en MD de recordame

--- middleware/recordameMiddleware.js
+++ middleware/recordameMiddleware.js
@@ -1,26 +1,26 @@
 function recordameMiddleware(req, res, next) {
-     next();
+  next();
 
-     if(req.cookies.recordame != undefined && req.session.usuarioLogueado == undefined) {
-        if (errors.isEmpty()) {
-            let usersJSON = fs.readFileSync('./data/users.json', {encoding: "utf-8"});
-            let users;
-            if (usersJSON == "") {
-              users = [];
-            } else {
-              users = JSON.parse(usersJSON);
-            }
-    
-            for (let i = 0; i < usersJSON.length; i++) {
-              if(usuarios[i].email == req.cookies.recordame) {
-                usuarioALoguearse = usuarios[i]; //al principio habia un let
-    
-                  break;
-                }
-              }
+  if(req.cookies.recordame != undefined && req.session.usuarioLogueado == undefined) {
+     if (errors.isEmpty()) {
+         let usersJSON = fs.readFileSync('./data/users.json', {encoding: "utf-8"});
+         let users;
+         if (usersJSON == "") {
+           users = [];
+         } else {
+           users = JSON.parse(usersJSON);
+         }
+ 
+         for (let i = 0; i < usersJSON.length; i++) {
+           if(usuarios[i].email == req.cookies.recordame) {
+             usuarioALoguearse = usuarios[i]; //al principio habia un let
+ 
+               break;
+             }
+           }
 
-     } 
-     req.session.usuarioLogueado = usuarioALoguearse
+  } 
+  req.session.usuarioLogueado = usuarioALoguearse
 }
 }
 
