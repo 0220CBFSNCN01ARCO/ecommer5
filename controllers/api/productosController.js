2d727f875602a2dc90734301367712cc803d223c
yasucaria <ya_sucaria@hotmail.com>
2020-08-09 21:44:11 -0300

    se limita atributos que muestra api como pide sprint 7

--- controllers/api/productosController.js
+++ controllers/api/productosController.js
@@ -4,17 +4,21 @@ const db = require("../../database/models");
 const productosController = {
 list : function(req, res){
     db.Libro.findAll({
-        include: [{association: "categoria"}]
+        include: [{association: "categoria"}],
+        attributes: ['idlibros', 'titulo', 'descripcion'] 
     })
     .then(function(libros) {
         for (let i = 0; i < libros.length; i++) {
             libros[i].setDataValue("endpoint", "/api/products/" + libros[i].idlibros)
         }
+    
+        
         let respuesta = {
         meta: {
             status: 200,
             total: libros.length,
             url: "/api/products"
+          
         },
         data: libros
     };
--- controllers/api/usuariosController.js
+++ controllers/api/usuariosController.js
@@ -4,14 +4,18 @@ const db = require("../../database/models");
 const usuariosController = {
 list : function(req, res){
     db.Usuario.findAll({
+
+            attributes: ['idusuario', 'nombre', 'email'] 
     })
     .then(function(usuarios) {
         for (let i = 0; i < usuarios.length; i++) {
             usuarios[i].setDataValue("endpoint", "/api/users/" + usuarios[i].idusuario)
+            
             if(!usuarios) {
                 return res.status(404).json({ok: false, msg: 'No se encontró el usuario'})
             }
         }
+        
         let respuesta = {
         meta: {
             status: 200,
@@ -23,8 +27,29 @@ list : function(req, res){
         res.send(respuesta);
     });
     },
+detail: function(req, res){
+db.Usuario.findByPk(req.params.idusuario, {
+    attributes: ['idusuario', 'nombre', 'email', "avatar"] 
+})
+
+.then(function(usuario){
+    if(!usuario) {
+        return res.status(404).json({ok: false, msg: 'No se encontró el usuario'})
+    } else {
+        userDetail.setDataValue("avatar", `http://localhost:3030/images/users/${userDetail.avatar}`);
+
+        res.json(userDetail);
+    }
+})
+
+},
+
+
     find: function(req, res) {
-        db.Usuario.findByPk(req.params.idusuario)
+        db.Usuario.findByPk(req.params.idusuario,{
+            attributes: ['idusuario', 'nombre', 'email', "avatar"]
+        }
+          )
         .then(function(usuarios){
 
             res.json(usuarios)
